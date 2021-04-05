e846a2a05422d1d0eb3d41e7aaa52e305113b94c
migelmalajISII <73987669+migelmalajISII@users.noreply.github.com>
2021-04-05 17:47:09 +0200

    Modifiche Temporanee

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ const axios = require('axios');
 const token = configurazione.TokenTelegram;
 const bot = new TelegramBot(token, { polling: true });
 
-const ID = 24;
-
 var categorie = [];
-FillCategorie()
+//FillCategorie()
+
+var countdown = {}
 
 function ConnectDB() {
     const config = {
@@ -51,10 +51,31 @@ function FillCategorie() {
 
 }
 
+function RestartTimeOut(id) { // Stranamente Funziona Molto BUONO
+    if (countdown[id] != undefined) {
+        clearTimeout(countdown[id])
+    }
+    var callback = setTimeout(() => {
+        var conn = ConnectDB()
+        conn.query("UPDATE Partite SET Status=01 WHERE FKGruppo=? AND Status=00", id, (err, res) => {
+            console.log(res)
+            if (res[0].affectedRows > 0) {
+                bot.sendMessage("-" + id, "Partita avviata");
+            }
+            DisconnectDB(conn)
+        })
+    }, 30000);
+    countdown[id] = callback;
+}
+
+function NuovoRound(id) {
+
+}
+
 bot.onText(/\/start/, (msg) => {
     if (msg.chat.type == 'group') {
         var conn = ConnectDB()
-        conn.query("INSERT INTO utenti(Ruolo,IDTelegram) VALUES (0,?)", parseInt(ID), (err, result) => {
+        conn.query("INSERT INTO utenti(Ruolo,IDUtente) VALUES (0,?)", Math.abs(msg.chat.id), (err, result) => {
             if (err) {
                 if (err.code != 'ER_DUP_ENTRY') {
                     bot.sendMessage(msg.chat.id, "Errore: Bot Inattivo.\nRiprovare più tardi");
@@ -70,9 +91,9 @@ bot.onText(/\/start/, (msg) => {
 });
 
 bot.onText(/\/newgame/, (msg) => {
-    if (msg.chat.type == 'private') { //Converti ai gruppi
+    if (msg.chat.type == 'group') { //Converti ai gruppi
         var conn = ConnectDB()
-        conn.query("SELECT IDPartita FROM Partite WHERE FKGruppo=? AND isEnded=0", parseInt(31), (err, res) => {
+        conn.query("SELECT IDPartita FROM Partite WHERE FKGruppo=? AND Status=01 OR Status=00", Math.abs(msg.chat.id), (err, res) => {
             if (res.length) {
                 bot.sendMessage(msg.chat.id, "Una partita è già in corso");
             } else {
@@ -83,42 +104,69 @@ bot.onText(/\/newgame/, (msg) => {
             DisconnectDB(conn)
         })
     } else {
-        bot.sendMessage(msg.chat.id, "Comando disponibile per i gruppi");
+        bot.sendMessage(msg.chat.id, "Comando disponibile solo per i gruppi");
+    }
+});
+
+bot.onText(/\/setround (\d+)/, (msg, match) => {
+    if (msg.chat.type == 'group') { //Converti ai gruppi
+        var conn = ConnectDB()
+        conn.query("UPDATE Partite SET Round=? WHERE FKGruppo=? AND (Status=00 OR Status=01)", [match[1], Math.abs(msg.chat.id)], (err, res) => {
+            if (err) {
+                bot.sendMessage(msg.chat.id, "Errore: Bot Inattivo.\nRiprovare più tardi");
+                console.error("Errore nel database: " + err)
+            }
+            DisconnectDB(conn)
+        })
+    } else {
+        bot.sendMessage(msg.chat.id, "Comando disponibile solo per i gruppi");
     }
 });
 
 bot.onText(/\/partecipo/, (msg) => {
-    if (msg.chat.type == 'group') { //Convertire su gruppi
-        Promise((resolve, reject) => {
+    if (msg.chat.type == 'group') {
+        new Promise((resolve, reject) => {
             var conn = ConnectDB()
-            conn.query("INSERT INTO utenti(Ruolo,IDTelegram) VALUES (0,?)", parseInt(ID), (err, result) => {
+            conn.query("INSERT INTO utenti(Ruolo,IDUtente) VALUES (0,?)", msg.from.id, (err, result) => {
                 if (err) {
                     if (err.code != 'ER_DUP_ENTRY') {
                         reject(err);
                         return
-                    } else {
-                        conn.query("SELECT IDUtente FROM utenti WHERE IDTelegram=?", parseInt(ID), (errs, results) => {
-                            if (errs) {
-                                reject(err);
-                                return
-                            }
-                            resolve(results[0].IDUtente);
-                        })
                     }
-                } else {
-                    resolve(result.insertId);
                 }
+                resolve(msg.from.id);
                 DisconnectDB(conn)
             })
-        }).then(value => {
-
+        }).then(() => {
+            var conn = ConnectDB()
+            conn.query("SELECT IDPartita FROM partite WHERE FKGruppo=?", Math.abs(msg.chat.id), (err, result) => {
+                //Gestione della partita avviata o non esistente e dell'utente già in partita
+                if (err) {
+                    throw err
+                } else {
+                    /*conn.query("INSERT INTO partecipazione(FKUtente,FKPartita) VALUES (?,?)", [value, result[0].IDPartita], (errs, results) => {
+                        if (errs)
+                            throw errs;
+                    })*/
+                }
+                DisconnectDB(conn)
+                RestartTimeOut(Math.abs(msg.chat.id))
+                return true;
+            })
+        }).then((ok) => {
+            console.log(ok)
+            if (!ok)
+                throw new Error("Not OK")
+            bot.sendMessage(msg.chat.id, "*[" + msg.from.first_name + "](tg://user?id=" + msg.from.id + ")* Si è unito in questa partita", { parse_mode: 'Markdown' });
         }).catch(err => {
+            //! Warning: Gestire Duplicato nella partecipazione
+            console.log(err)
             bot.sendMessage(msg.chat.id, "Errore: Bot Inattivo.\nRiprovare più tardi");
             console.error("Errore nel database: ")
             throw err;
         })
     } else {
-        bot.sendMessage(msg.chat.id, "Comando disponibile per i gruppi");
+        bot.sendMessage(msg.chat.id, "Comando disponibile solo per i gruppi");
     }
 });
 
@@ -129,7 +177,7 @@ bot.on("callback_query", function(callbackQuery) {
     let number = callbackQuery.data.split('_')[1].trim()
     let categoria = categorie.find(elem => elem.callback_data == callbackQuery.data).text;
     var conn = ConnectDB()
-    conn.query("INSERT INTO Partite(FKCategoria,FKGruppo) VALUES (?,?)", [parseInt(number), parseInt( /*callbackQuery.message.chat.id*/ 31)], (err, results) => {
+    conn.query("INSERT INTO Partite(FKCategoria,FKGruppo) VALUES (?,?)", [parseInt(number), Math.abs(parseInt(callbackQuery.message.chat.id))], (err, results) => {
         if (err) {
             bot.editMessageText("Errore: Bot Inattivo.\nRiprovare più tardi", {
                 chat_id: callbackQuery.message.chat.id,
